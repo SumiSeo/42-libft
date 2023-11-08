d7583e8d8ac867b9e4c1ca5d9b0f7e81a2953343
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-08 14:18:34 +0100

    fix/ft-is-alpha-norminette

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,14 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:16:04 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/08 14:18:16 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdio.h>
 #include <ctype.h>
 
- 
-int ft_isalpha(char word){
-    
-        if((word >= 'a' && word <='z') || (word >='A' && word <= 'Z'))
-            return 1024;
-        else 
-            return 0;
- }
+int	ft_isalpha(char word)
+{
+	if ((word >= 'a' && word <= 'z') || (word >= 'A' && word <= 'Z'))
+		return (1024);
+	else
+		return (0);
+}
 
 int main(){
     int num = 'a';

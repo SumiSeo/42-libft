fb0e6311195bae3b411672762733cd709f6ff14e
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-08 14:44:03 +0100

    feat/fix-is-memcpy-norminettE

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,19 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:40:59 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/08 14:43:44 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <string.h>
 #include <stdio.h>
 
-char    *ft_memcpy(char *dest, char *src, size_t n)
+char	*ft_memcpy(char *dest, char *src, size_t n)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while(i < (int) n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-     return dest;
+	i = 0;
+	while (i < (int) n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
-
 
 int main()
 {

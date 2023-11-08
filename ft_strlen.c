1004a068511c4f35fc8171283271151d6d8f703e
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-08 14:46:56 +0100

    fix/fix-func-strlen-norminette

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:44:13 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/08 14:46:33 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdio.h>
 #include <string.h>
 
-int ft_strlen(char *words)
+int	ft_strlen(char *words)
 {
-    int length = 0;
-    while(*words != 0){
-        words ++;
-        length ++;
-    }
-    return length;
+	int	length;
+
+	length = 0;
+	while (*words != 0)
+	{
+		words ++;
+		length ++;
+	}
+	return (length);
 }
 
 int main()

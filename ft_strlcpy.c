619152308850517a889ca34ed2d40f1433c65106
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-10 14:12:11 +0100

    fix/fix-strlcpy-function

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,34 +6,37 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:56:31 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/08 16:11:27 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/10 14:11:47 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
 #include <stdio.h>
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int	i;
+	size_t	length;
+	size_t	i;
 
+	length = 0;
 	i = 0;
-	if ((int) size < 0)
+	while (src[length] != '\0')
+		length++;
+	while (*dst && *src && i < size)
 	{
-		return (0);
-	}
-	while (i < (int) size)
-	{
-		dest[i] = src[i];
+		if (i == size - 1)
+			dst[i] = '\0';
+		else
+			dst[i] = src[i];
 		i++;
 	}
-	return (i);
+	return (length);
 }
 
 int	main()
 {
-	char	source[] = "Sumi is writing code";
-	char	dest[] = "What are you doing";
-	size_t	size = 4;
+	char	source[] = "Hello there, Venus";
+	char	dest[100];
+	size_t	size = 0;
 
 	ft_strlcpy(dest,source,size);
 }

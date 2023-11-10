813fafeb9cd4b3537b1c3c90d4baed7db3ef656c
Sumi Seo <sumseo@paul-f4Ar5s6.clusters.42paris.fr>
2023-11-10 18:27:41 +0100

    feat/finish-to-lower-funcc

---
+++ ft_tolower.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:25:03 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/10 18:27:26 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int	ft_tolower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (c + 32);
+	else
+		return (c);
+}
+
+int	main()
+{
+	char	alpha = 'Z';
+	printf("%c\n",tolower(alpha));
+	printf("%c\n",ft_tolower(alpha));
+}

4ee4ebaedee96f4f543d21f98af3f3c1c667a58a
Sumi Seo <sumseo@paul-f4Ar5s6.clusters.42paris.fr>
2023-11-10 18:46:30 +0100

    feat/finishi-ft-strchr

---
+++ ft_strchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:28:03 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/10 18:46:15 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			break ;
+		s++;
+	}
+	return ((char *) s);
+}
+
+int	main()
+{
+	char str[] = "hello world";
+	char character = 'o';
+	printf("returned character -> %s\n", strchr(str,character));
+	printf("returned character -> %s\n", ft_strchr(str,character));
+
+}

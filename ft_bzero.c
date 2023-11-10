6358d397819c97a9c75d2a195871deb6de4e1157
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-10 11:27:01 +0100

    fix/prototype-for-ft-bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,21 +6,23 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:11:59 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/08 14:15:02 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/10 11:26:41 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <strings.h>
 #include <stdio.h>
 
-void	ft_bzero(char *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	int	i;
+	size_t	i;
+	char	*casted_pointer;
 
+	casted_pointer = (char *) s;
 	i = 0;
-	while (i < (int) n)
+	while (i < n)
 	{
-		s[i] = '\0';
+		*casted_pointer = '\0';
 		i++;
 	}
 }
@@ -28,12 +30,12 @@ void	ft_bzero(char *s, size_t n)
 int main()
 {
     char str[] = "TESTis for Testing"; 
-    printf("original function -> %s\n", str);
+    printf("original function -> %s <-\n", str);
     bzero(str, 1);
-    printf("original function -> %s\n", str);
+    printf("original function -> %s <-\n", str);
     char str2[] = " I am testing this one";
-    printf("my function-> %s\n",str2);
+    printf("my function-> %s <-\n",str2);
     ft_bzero(str2, 1);
-    printf("my function -> %s\n", str2);
+    printf("my function -> %s <-\n", str2);
 
 }

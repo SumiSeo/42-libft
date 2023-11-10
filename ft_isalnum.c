af77c2173dfcdec9b28b53fee7c1c961f8d0fd0b
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-10 09:46:35 +0100

    fix/ft-alnum-prototype

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,16 +6,17 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:47:11 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/08 14:53:59 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/10 09:45:01 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <ctype.h>
 
-int	ft_isalnum(char c)
+int	ft_isalnum(int character)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-		|| (c >= '0' && c <= '9'))
+	if ((character >= 'a' && character <= 'z')
+		|| (character >= 'A' && character <= 'Z')
+		|| (character >= '0' && character <= '9'))
 		return (8);
 	else
 		return (0);
@@ -23,7 +24,7 @@ int	ft_isalnum(char c)
 
 int main()
 {
-    char test = '$';
+    char test = '0';
     printf("original function -> %d\n",isalnum(test));
     printf("my function -> %d\n",ft_isalnum(test));
 }

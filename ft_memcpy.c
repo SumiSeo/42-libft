a66d65da493b5a917a25ff1c9cfec6e877b74336
Sumi Seo <sumseo@paul-f4Br6s2.clusters.42paris.fr>
2023-11-10 13:11:00 +0100

    fix/fix-prototype-memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,28 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:40:59 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/08 14:43:44 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/10 13:10:39 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
 #include <stdio.h>
 
-char	*ft_memcpy(char *dest, char *src, size_t n)
-{
-	int	i;
+void	*ft_memcpy(void *destination, const void *source, size_t size)
 
+{
+	size_t	i;
+	char	*casted_destination;
+	char	*casted_source;
+
+	casted_source = (char *) source;
+	casted_destination = (char *)destination;
 	i = 0;
-	while (i < (int) n)
+	while (i < size)
 	{
-		dest[i] = src[i];
+		casted_destination[i] = casted_source[i];
 		i++;
 	}
-	return (dest);
+	return (casted_destination);
 }
 
 int main()
@@ -31,7 +36,6 @@ int main()
     char dest[256];
     size_t size = 4;
 
-    printf("my function destination -> %s\n",ft_memcpy(dest,src,size));
-    
+    printf("my function destination -> %p\n",ft_memcpy(dest,src,size));
     
 }

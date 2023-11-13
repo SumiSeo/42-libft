3ec895a8085b860905a2c5918be5c8d2ccd1d704
Sumi Seo <sumseo@paul-f4Br6s1.clusters.42paris.fr>
2023-11-13 10:12:56 +0100

    feat/finish-func-atoi-and-strnstr

---
+++ ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 09:24:18 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/13 10:12:44 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_atoi(const char*nbtr)
+{
+	int		i;
+	long	nb;
+	int		sign;
+
+	sign = 1;
+	i = 0;
+	nb = 0;
+	if (!nbtr)
+		return (0);
+	while (nbtr[i] != '\0')
+	{
+		if (nbtr[0] == '-')
+			sign *= -1;
+		if (nbtr[1] == '-' || nbtr[1] == '+')
+			return (0);
+		if (nbtr[i] >= '0' && nbtr[i] <= '9')
+			nb = (nb * 10) + (nbtr[i] - '0');
+		i++;
+	}
+	return (sign * nb);
+}
+
+int	main()
+{
+	char	*str = "590310";
+	printf("original function ->%d\n",atoi(str));
+	printf("my function ->%d\n",ft_atoi(str));
+
+}
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,43 +6,39 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 17:46:57 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/11 18:22:25 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/13 09:22:43 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	size_t	j;
-	i = 0 ;
+	int		j;
 
-	while(i < len && big[i])
+	i = 0 ;
+	while (*big != '\0' && i < len)
 	{
 		j = 0;
-		while(little[j] != '\0')
+		while (little[j] != '\0')
 		{
-			if(little[j] == big[i])
-			{
-				printf("found little word %c\n", little[j]);
-				printf("found big word %c\n", big[i]);
-			}
+			if (little[j] == big[i + j])
+				return ((char *) big);
 			j++;
+			i++;
+			big++;
 		}
-
-		i++;
 	}
-	return "hi";
-
+	return (NULL);
 }
 
 int	main()
 {
-	  const char *largestring = "Foo Bar Baz";
-      const char *smallstring = "Bar";
-      size_t size = 4;
+	  char *largestring = "Hello world";
+      char *smallstring = "lo";
+      size_t size = 10;
 	  printf("returned value -> %s\n", ft_strnstr(largestring, smallstring, size));
 
 }

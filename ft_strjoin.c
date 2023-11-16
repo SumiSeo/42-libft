d314cc2161f22d4808e7c79c9275a59f6e1e6496
Sumi Seo <sumseo@paul-f4Ar6s1.clusters.42paris.fr>
2023-11-16 09:58:44 +0100

    feat/finalize-fun-strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 09:55:21 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/16 09:58:29 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,9 +24,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	length = 0;
-	if(! (*s1&& *s2))
+	if (! (*s1&& *s2))
 		return NULL;
-
 	while(s1[i] || s2[j])
 	{
 		if(s1[i])
@@ -31,27 +42,30 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = (char *)malloc(length * sizeof(char));
 	i = 0;
 	j = 0;
-	while(*s1)
+	while(*s1 || *s2)
 	{
-		str[i] = *s1;
-		s1++;
-		i++;
+		if(*s1)
+		{
+			str[i] = *s1;
+			s1++;
+			i++;
+		}
+		else if (*s2)
+		{	
+			str[i+j] = *s2;
+			s2++;
+			j++;
+
+		}
 	}
-	while(*s2)
-	{
-		str[i+j] = *s2;
-		s2++;
-		j++;
-	}
-	
-	return str;
+	return (str);
 }
 
 
 int	main()
 {
-	char	*s1 = "Jack";
-	char	*s2 = "Harlow";
+	char	*s1 = "42";
+	char	*s2 = "ecole";
 
 	printf("my function -> %s\n", ft_strjoin(s1,s2));
 

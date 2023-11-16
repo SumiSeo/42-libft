96fc94e9e26a1efe5bffc6a41367c112f04a5a58
Sumi Seo <sumseo@paul-f4Ar6s1.clusters.42paris.fr>
2023-11-16 09:50:58 +0100

    feat/finish-func-strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,32 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 09:49:31 by sumseo            #+#    #+#             */
+/*   Updated: 2023/11/16 09:50:36 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-	int	length;
-	int i;
+	int		length;
+	int		i;
 	char	*casted_str;
 
 	length = 0;
 	i = 0;
-	while(s1[length])
-	{
+	while (s1[length])
 		length++;
-	}
 	casted_str = (char *) malloc(length * sizeof(char));
-	
-	while(s1[i])
+	while (s1[i])
 	{
 		casted_str[i] = s1[i];
 		i++;
 	}
-	return casted_str;
+	return (casted_str);
 }
 
 int	main()
 {
-	char *str = "This is sufficient memory";
+	char *str = "";
 
 	printf("original function %s\n", strdup(str));
 	printf("original function %p\n", strdup(str));

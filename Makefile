bf1d0c30c717053b7de2fc6033ce9c755529c701
Sumi Seo <sumseo@paul-f4Br6s8.clusters.42paris.fr>
2023-11-21 10:19:23 +0100

    feature/modify-make-file

--- Makefile
+++ Makefile
@@ -1,23 +1,26 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror -g3
 NAME = libft.a
-SOURCE = $(wildcard *.c)
+SOURCE = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+			ft_strlen.c ft_memset.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c \
+			ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c \
+			ft_strchr.c ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c \
+			ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c
 OBJ = $(SOURCE:.c=.o)
-
+BONUS_SOURCE = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c \
+				ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c\
+				ft_lstmap.c
+BONUS_OBJ = $(BONUS_SOURCE:.c=.o)
+RM = rm -f
 all : $(NAME)  
 
 $(NAME) : $(OBJ)
 	ar rc $(NAME) $(OBJ)
 
-%.o : %.c
-	$(CC) -o $@ -c $< $(CFLAGS)
+bonus : $(OBJ) %(BONUS_OBJ)
+	ar rc $(NAME) $(OBJ) $(BONUS_OBJ)
 
 clean :
-	rm -rf *.o
-
-fclean : clean
-	rm -rf $(NAME)
-
-re : fclean all 
+	$(RM) $(OBJ) $(BONUS_OBJ)
 
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:15:08 by sumseo            #+#    #+#             */
-/*   Updated: 2023/11/13 11:17:45 by sumseo           ###   ########.fr       */
+/*   Updated: 2023/11/21 10:17:39 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,51 +20,58 @@
 # include <string.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 // return int
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_atoi(const char *nbtr);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+int					ft_atoi(const char *nbtr);
 
 // return void
 
-void	ft_bzero(void *s, size_t n);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void				ft_bzero(void *s, size_t n);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 // return void pointer
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_calloc(size_t count, size_t size);
+void				*ft_memset(void *s, int c, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
+void				*ft_calloc(size_t count, size_t size);
 
 // return size_t
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t				ft_strlen(const char *s);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
 
 // return string
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*ft_itoa(int n);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+char				*ft_strnstr(const char *big, const char *little,
+						size_t len);
+char				*ft_itoa(int n);
+char				*ft_strdup(const char *s1);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				*ft_strtrim(char const *s1, char const *set);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
 
 // return pointerof character
-char	**ft_split(char const *s, char c);
+char				**ft_split(char const *s, char c);
 
 #endif
